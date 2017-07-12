ca7c34b0cbacb4cdee7b21bfe6c2bfa3ee4d7c85
Sateesh Peetha <sateesh_peetha@yahoo.com>
2017-07-12 21:18:46 +0530

    added separate init function to make it nxn tic tac toe AI game

--- tictactoeAI.js
+++ tictactoeAI.js
@@ -1,30 +1,89 @@
 var app = angular.module('tictactoe', []);
 
 function boardController($scope) {
-  
-  
-  $scope.boardSize = 3;
-  $scope.boardLength = $scope.boardSize * $scope.boardSize;
-  
-  $scope.maxWins =  $scope.boardSize + $scope.boardSize + 2;
-    
-  $scope.board = [];
-  $scope.wins = {};
-  
-  $scope.player = "x";
-  $scope.hPlayer = "x";
-  $scope.cPlayer = "c";
- 
-  //init board
-  
-  for(i=0;i<$scope.boardLength;i++) {
-    
-    board[i] = i;
-    
-  }
-  
 
-  
+
+  init = function($scope, boardSize, callback) {
+
+    $scope.boardSize = boardSize;
+    $scope.boardLength = $scope.boardSize * $scope.boardSize;
+
+    $scope.maxWins = $scope.boardSize + $scope.boardSize + 2;
+
+    $scope.board = [];
+    $scope.wins = [];
+
+    $scope.player = "x";
+    $scope.hPlayer = "x";
+    $scope.cPlayer = "c";
+
+    callback($scope);
+
+    for (i = 0; i < $scope.boardLength; i++) {
+      $scope.board[i] = i;
+    }
+
+    i = 0
+
+    for (j = 0; j < $scope.boardLength; j = j + $scope.boardSize) {
+      win = []
+
+      for (k = 0; k < $scope.boardSize; k++) {
+        win[k] = k + j;
+      }
+
+      $scope.wins[i] = win;
+      i++;
+    }
+
+    for (l = 0; l < $scope.boardSize; l++) {
+      win = [];
+      k = l;
+      for (j = 0; j < $scope.boardSize; j++) {
+        win[j] = k + j;
+        k = k + ($scope.boardSize - 1);
+      }
+
+      $scope.wins[i] = win;
+      i++;
+    }
+
+    k = 0;
+    win = []
+    for (j = 0; j < $scope.boardSize; j++) {
+
+      win[j] = k;
+      k = k + $scope.boardSize + 1;
+
+    }
+
+    $scope.wins[i] = win;
+    i++;
+
+    k = $scope.boardSize - 1;
+    win = []
+    for (j = 0; j < $scope.boardSize; j++) {
+
+      win[j] = k;
+      k = k + ($scope.boardSize - 1);
+
+    }
+
+    $scope.wins[i] = win;
+
+  }
+
+  //init board
+
+  init($scope, 3, function($scope) {
+    // console.log("log  ")
+    //  console.log($scope.wins)
+    //  console.log($scope.board)
+
+  })
+
+
+
 }
 
 
